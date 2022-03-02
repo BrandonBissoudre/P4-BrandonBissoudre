02243a7374e172a91cc7d625dc0074aa81386cd0
BrandonBissoudre <93727905+BrandonBissoudre@users.noreply.github.com>
2022-03-02 11:55:01 +0100

    verifmail

--- modal.js
+++ modal.js
@@ -72,3 +72,14 @@ function verifnom(nom) {
   }
 
 }
+
+
+function verifemail(emailAdress) {
+
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailAdress.match(regexEmail)) {
+    return true;
+  } else {
+    return false;
+  }
+}
